0709a84dea01acb13405520efd21d899f5077982
Simon Gorozabel <sgorozabel@gmail.com>
2023-08-27 10:29:16 -0500

    finishing

--- app/(routes)/category/[categoryId]/page.tsx
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -48,11 +48,14 @@ const CategoryPage: React.FC<CategoryPageProps> = async ({
               <Filter valueKey="colorId" name="Colors" data={colors} />
             </div>
             <div className="mt-6 lg:col-span-4 lg:mt-0">
-              {products.length === 0 && <NoResults />}
+              {Array.isArray(products) && products.length === 0 && (
+                <NoResults />
+              )}
               <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-                {products.map((item) => (
-                  <ProductCard key={item.id} data={item} />
-                ))}
+                {Array.isArray(products) &&
+                  products.map((item: any) => (
+                    <ProductCard key={item.id} data={item} />
+                  ))}
               </div>
             </div>
           </div>
