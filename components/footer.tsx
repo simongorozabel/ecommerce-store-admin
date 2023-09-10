0079afec0d1768279279fb8612d68642a066d79d
Simon Gorozabel <sgorozabel@gmail.com>
2023-09-09 20:03:23 -0500

    brand changes

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
     <footer className="bg-white border-t">
       <div className="mx-auto py-10">
         <p className="text-center tex-xs text-black">
-          &copy; 2021 Aizzle, Inc. All rights reserved.
+          &copy; 2023 BRAND, Inc. No rights reserved.
         </p>
       </div>
     </footer>
